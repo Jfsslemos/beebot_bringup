2dd3f5534e2cb4a00c5b31d74cfe66d099f7f5a0
jfsslemos <jfsslemos@gmail.com>
2023-10-19 10:15:12 -0300

    first structure for the beebot launch package

---
+++ launch/beebot_recognition.py
@@ -0,0 +1,23 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    config = os.path.join(
+        get_package_share_directory('beebot_bringup'),
+        'config',
+        'params.yaml'
+        )
+        
+    node=Node(
+        package = 'beebot_recognition',
+        name = 'your_amazing_node',
+        executable = 'corrosion_recognition',
+        parameters = [config]
+    )
+
+    ld.add_action(node)
+    return ld
